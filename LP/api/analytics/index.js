f3d7899db6a86e77ae523868ed9e60b724e0f3ae
Nicanor008 <nicanorkorir008@gmail.com>
2020-06-01 18:44:13 +0300

    Remove LP due to a lot of secret keys exposed, will add in future

--- LP/api/analytics/index.js
+++
@@ -1,9 +0,0 @@
-const express=require('express')
-
-const analyticsController = require('./analytics_controller')
-
-const router = express.Router()
-
-router.route('/todo').get(analyticsController.getAnalytics)
-
-module.exports = router
--- LP/api/users/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const users = require("./users_controllers")
-
-const router = express.Router()
-
-router.route("/").get(users.fetchAllUsers)
-
-module.exports = router
